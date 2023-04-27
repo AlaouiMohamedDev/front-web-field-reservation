15f2cdbe251dcbc9a3b7d36fb97dc8e708c3baa5
therealmoro <sayrosse1@gmail.com>
2023-04-27 23:53:30 +0200

    globaml fix prob url https

--- components/global.js
+++ components/global.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://kritirankk.pythonanywhere.com";
+const BASE_URL = "https://kritirankk.pythonanywhere.com";
 export default BASE_URL;
  //http://kritirankk.pythonanywhere.com
  //http://127.0.0.1:8000
